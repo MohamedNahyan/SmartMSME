4209599ed4c58740c513da4c927e30ed5518f4cc
Nahyan <mohamednahyan11@gmail.com>
2026-03-06 14:32:16 +0530

    update auth serializer

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -102,15 +102,19 @@ class ReminderSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = ["id", "username", "email", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
-            password=validated_data["password"]
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data.get("last_name", "")
         )
         return user
